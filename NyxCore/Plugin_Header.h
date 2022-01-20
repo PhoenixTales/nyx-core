c809a9f4876fa0ec659ee5fb04e46d61e1f89766
Flosha <fnp@posteo.de>
2022-01-20 21:32:47 +0100

    rename NyxCore to AlphaAttributes for coming plugin differentiation 2

--- NyxCore/Plugin_Header.h
+++
@@ -1,8 +0,0 @@
-// Header file
-// Multi-platform file can not have `#pragma once`!!!
-
-// dynamic namespace
-namespace NAMESPACE {
-  // TO DO
-  // Structures, prototypes, externs and etc . . .
-}
--- NyxCore/Plugin_Source.hpp
+++
@@ -1,9 +0,0 @@
-// Source file
-// Multi-platform file can not have `#pragma once`!!!
-// For compile plugin in Multiplatform mode - select 'Debug' or 'Release' configuration.
-
-// dynamic namespace
-namespace NAMESPACE {
-  // TO DO
-  // Definitions and global data
-}
